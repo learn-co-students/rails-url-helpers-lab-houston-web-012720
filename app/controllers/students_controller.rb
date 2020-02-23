35e4f5f1033998dc316dd2c96f254212dff5e8d3
GianMoran <gpmoran@uh.edu>
2020-02-23 15:15:40 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,7 +7,12 @@ class StudentsController < ApplicationController
 
   def show
   end
-
+  def activate
+    @student = set_student
+    @student.active = !@student.active
+    @student.save 
+    redirect_to student_path(@student)
+  end
   private
 
     def set_student
