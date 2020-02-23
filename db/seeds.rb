8905b07529f5449637d52499b955a0a99f77703c
somaia-khalil <sumaya_khair@live.com>
2020-02-23 12:18:02 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+s1 = Student.create(first_name: "Hamza", last_name: "Flan")
+s2 = Student.create(first_name: "Somai", last_name: "Khalil")
