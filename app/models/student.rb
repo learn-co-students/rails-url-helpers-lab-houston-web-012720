bc933746d07025574d8bd1f9c561fb382cac6ce1
placidowang <placidow@gmail.com>
2020-02-24 01:21:04 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,10 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def toggle_active
+    # self.active = !self.active
+    # self.save
+    self.update(active: !self.active)
+  end
 end
